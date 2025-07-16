e963ce94d99729d084866bde17da05b5de487703
elipticalDrive <nebulous.machinery@gmail.com>
2025-07-16 14:27:08 -0400

    e

--- workers/write-api.ts
+++ workers/write-api.ts
@@ -1,17 +1,20 @@
+import * as EmailValidator from 'email-validator';
+
 export async function createAccount(env: Env, email: string, name: string, password: string): Promise<Response> {
 	const id = crypto.randomUUID();
 
+	if (!EmailValidator.validate(email)) return new Response("Invalid email address", { status: 400 });
+
 	// check if account with email already exists
-	const checkStatement = env.DB.prepare("SELECT COUNT(*) FROM users WHERE email = ?");
+	const checkStatement = env.DB.prepare('SELECT COUNT(*) FROM users WHERE email = ?');
 	const result = await checkStatement.bind(email).first();
 	const count = result?.['COUNT(*)'] as number | null;
 
 	if (count && count > 0) return new Response("Account with that email already exists", { status: 409 });
 
-	const statement = env.DB.prepare("INSERT INTO users (id, name, email, password) VALUES ('?', '?', '?', '?')");
-
-	// console.log(id);
-	statement.bind(id, name, email, password);
+	const statement = env.DB
+		.prepare('INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)')
+		.bind(id, name, email, password);
 
 	try {
 		await statement.run();
